be41a7d0319af6318e7eb41474d94a7238f5b749
sonu-simon <sonuisplaying@gmail.com>
2022-07-19 11:27:41 +0530

    db fetch works and prints in jsp

--- WEB-INF/src/java/learn/LoginForm.java
+++ WEB-INF/src/java/learn/LoginForm.java
@@ -37,17 +37,17 @@ public class LoginForm implements SessionAware{
             sessionMap.put("username",username);  
             
             try {
-            //     Class.forName("com.mysql.jdbc.Driver");  
-            //     Connection con = DriverManager.getConnection(  
-            //     "jdbc:mysql://localhost:3306/testdb?characterEncoding=utf8","root","password");  
-            //     Statement stmt = con.createStatement();  
-            //     ResultSet rs = stmt.executeQuery("select * from users"); 
-            //     while(rs.next()){
-            //         System.out.println(rs.getString(1));
-            //         sessionMap.put("result",rs.getString(1));  
+                Class.forName("com.mysql.jdbc.Driver");  
+                Connection con = DriverManager.getConnection(  
+                "jdbc:mysql://localhost:3306/testdb?characterEncoding=utf8","root","password");  
+                Statement stmt = con.createStatement();  
+                ResultSet rs = stmt.executeQuery("select * from users"); 
+                while(rs.next()){
+                    System.out.println(rs.getString(1));
+                    sessionMap.put("result",rs.getString(1));  
 
-            // }
-            // con.close();
+            }
+            con.close();
             } catch (Exception e) {
                 
             }
